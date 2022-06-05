6af41ff15fcdcd577498a820eb946fdb81e229f5
George <gprmd62@gmail.com>
2022-06-05 11:33:24 +0100

    - added apexcharts - responsive layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,59 @@
-console.log("I Live!")
+import ApexCharts from 'apexcharts'
+
+// Generate chart
+var options = {
+    chart: {
+      height: 200,
+      type: "radialBar",
+      fontFamily: 'Bebas Neue'
+    },
+  
+    series: [78.9],
+    colors: ["#5900D7"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "70%",
+          background: "#F1E9FD"
+        },
+        track: {},
+        dataLabels: {
+          value: {
+            color: "#000",
+            fontSize: "18px",
+            show: true,
+            offsetY: -10,
+            offsetX: -10,
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#5900D7"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+
+    },
+    labels: [""],
+    responsive: [{
+        breakpoint: 768,
+        options: {
+            chart: {
+                height: 150
+            }
+        },
+    }]
+    
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  
+  chart.render();
+  
